6f95adadbbc00608dd8237a13d024e79936fd06a
Amanda <amanderson@csumb.edu>
2017-03-28 03:56:38 +0000

    fb login

--- react/App.js
+++ react/App.js
@@ -1,13 +1,25 @@
 import * as React from 'react';
+//import { Button } from './Button';
+
 
 export class App extends React.Component {
     
     render() {
-        return <div>
+        return( <div>
                     <div className="plantBotApp">
                         <h1 className="banner">Hello from PlantBot!</h1>
+                             <div
+                        className="fb-login-button"
+                        data-max-rows="1"
+                        data-size="medium"
+                        data-show-faces="false"
+                        data-auto-logout-link="true">
                     </div>
-                </div>;
+                    <Button />                   
+
+                    </div>
+                </div>
+                );
     }
 }
 
