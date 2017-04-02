5190505e4a54ecd36b04abacfa18989ce664b11b
david keller <dakeller@csumb.edu>
2017-04-02 21:25:19 +0000

    created the plant database

--- models.py
+++ models.py
@@ -1,28 +1,38 @@
 import flask_sqlalchemy, app, os
 
 # REMOTE APPLICATION
-app.app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+#app.app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 
 # CLOUD9 DEVELOPMENT ENVIRONMENT
-#app.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:pass@localhost/messaging'
+app.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://firefly:password@localhost/postgres'
 
 # Setup connection to DB system
 db = flask_sqlalchemy.SQLAlchemy(app.app)
 print "\"Models\" connected and DB import complete!"
 
 # "TextObj" DB Model
-class TextObj(db.Model):
+class plants(db.Model):
     
     # Auto-Increment Primary Key Integer
     id = db.Column(db.Integer, primary_key=True)
     
     # Setup other columns
-    text = db.Column(db.String(160))
+    img = db.Column(db.String(200))
+    userid = db.Column(db.Integer)
+    name = db.Column(db.String(60))
+    location = db.Column(db.String(60))
+    date = db.Column(db.DateTime)
+    ifedit = db.Column(db.String(20))
+    editdate = db.Column(db.String(60))
     
     # Constructor from Python
-    def __init__(self, text):
-        self.text = text
+    def __init__(self, img, userid, name, location, date):
+        self.img = img
+        self.userid = userid
+        self.date = date
+        self.name = name
+        self.location = location
     
     def __repr__(self):
-        return "Obj.text: %s" % self.text
+        return "Obj.plantName: %s" % self.plantName
         
