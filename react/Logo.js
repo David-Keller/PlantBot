de65ba9c075bd63bfcd6c67308e51ee69432ad5f
Andrew <APorter@CSUMB.edu>
2017-04-05 02:43:35 +0000

    Logo Push

---
+++ react/Logo.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export class Logo extends React.Component {
+    
+    render() {
+        return(
+            <img src="http://www.herbhedgerow.co.uk/herb/wp-content/uploads/2013/04/Nettle-Leaf.png"></img>
+            );
+    }
+}
+
