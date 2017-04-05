1e1c3c31e892e67836383b061bfc547ea8e9bab8
Andrew <APorter@CSUMB.edu>
2017-04-05 02:46:08 +0000

    Fix

--- react/App.js
+++ react/App.js
@@ -3,12 +3,14 @@ import * as React from 'react';
 import { Banner } from './Banner';
 import { FaceBook } from './FaceBook';
 import { Intro } from './Intro';
+import { Logo } from './Logo';
 
 export class App extends React.Component {
     
     render() {
         return(<div className="plantBotApp">
                     <Banner />
+                    <Logo />
                     <FaceBook />
                     <Intro />
                 </div>
