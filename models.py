b0363793a1660c724879985e761a2798e6efd333
Andrew <APorter@CSUMB.edu>
2017-04-25 04:38:41 +0000

    Added production DB switch

--- models.py
+++ models.py
@@ -3,11 +3,10 @@ from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method
 from sqlalchemy import func
 import math
 
-# REMOTE APPLICATION
-# app.app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-
-# CLOUD9 DEVELOPMENT ENVIRONMENT
-app.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:pass@localhost/plantbot'
+if os.getenv("CIRCLE_CI_TEST_ENV") != "TRUE":
+    app.app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:pass@localhost/plantbot'
 
 # Setup connection to DB system
 db = flask_sqlalchemy.SQLAlchemy(app.app)
