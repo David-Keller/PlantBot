3e2fe19c9008b040f3686c03479db2b824fd5d74
Andrew <APorter@CSUMB.edu>
2017-04-25 19:00:36 +0000

    Still tweaking prod DB

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
 import flask_sqlalchemy, app, os
 import math
 
-#app.app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-app.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:pass@localhost/plantbot'
+app.app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+#app.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:pass@localhost/plantbot'
 db = flask_sqlalchemy.SQLAlchemy(app.app)
 print "\"Models\" connected and DB import complete!"
 
