e2a00e13dda7d38a96b49992f6cbd339daae70f6
Andrew <APorter@CSUMB.edu>
2017-05-05 06:16:43 +0000

    Canvas conversion in Post

--- react/Post.js
+++ react/Post.js
@@ -4,17 +4,12 @@ export class Post extends React.Component {
     constructor(props){
         super(props);
     }
-    
-    render(){
-        console.log("Post starting render...");
-        console.log("State:");
-        console.log(this.state);
-        console.log("Props:");
-        console.log(this.props);
-        var canvas = new Canvas();
-        if(this.props.post.URI != null){
+    componentDidMount(){
+    var $this = $(ReactDOM.findDOMNode(this));
+    if(this.props.post.URI != null){
+            var canvas = document.getElementById("canvas" + this.props.id);
             var context = canvas.getContext('2d');
-            var img = new Image();
+            img = new Image();
             img.onload = function() {
                 if(img.width > canvas.width){
                     let ratio = img.width/200;
@@ -31,13 +26,21 @@ export class Post extends React.Component {
             }
             img.src = this.props.post.URI;
         }
+    }
+    render(){
+        console.log("Post starting render...");
+        console.log("State:");
+        console.log(this.state);
+        console.log("Props:");
+        console.log(this.props);
+        let img = (<canvas style={{margin:"auto"}} className="canvas" id={"canvas" + this.props.id} />)
         return(
 
             <div className="post">
                 <h3><strong>Search Results</strong></h3>
                 <h3> {this.props.post.name} {this.props.post.user} {this.props.post.date} </h3>
                 <br/>
-                {canvas}
+                {img}
             </div>
             );
     }
